e1c598cf7ccaa98a3267424d4a8b2372cc25abdc
Yasmin Oliveira <yasminfigueiredo892@yahoo.com>
2021-05-10 20:58:18 -0300

    Play Dice

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -12,15 +12,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var dadoImageView1: UIImageView!
     @IBOutlet weak var dadoImageView2: UIImageView!
+
+
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        dadoImageView1.image = #imageLiteral(resourceName: "DiceFour")
+    @IBAction func rolarDadosPressed(_ sender: UIButton) {
         
-        dadoImageView1.alpha = 0.5
-
-
+        let arrayDados = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"),UIImage(named: "DiceSix"),]
+     
+        dadoImageView1.image = arrayDados[Int.random(in: 0...5)]
+        dadoImageView2.image = arrayDados[Int.random(in: 0...5)]
+       
     }
     
 }
+
+
