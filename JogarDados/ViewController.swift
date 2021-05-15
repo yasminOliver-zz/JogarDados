db2c264a854365d33853fff7120f2dc818fbe675
Leila Morais <leilasmorais@hotmail.com>
2021-05-15 17:08:23 -0400

    Finalizando Projeto

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -13,15 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var dadoImageView1: UIImageView!
     @IBOutlet weak var dadoImageView2: UIImageView!
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
-        dadoImageView1.image = #imageLiteral(resourceName: "DiceFour")
         
-        //dot notation: who.what = value (quem.oque=valor)
-        dadoImageView1.alpha = 0.5
     }
 
+    //MARK: - Action
+    @IBAction func rolarDadosprassed(_ sender: UIButton) {
+        
+        let arrayDados = [UIImage(named: "DiceOne"), UIImage(named:"Dicetwo"), UIImage(named:"DiceThree"),UIImage(named:"DiceFour"), UIImage(named:"DiceFive"), UIImage(named:"DiceSix")]
 
+        dadoImageView1.image = arrayDados [Int.random(in: 0...5)]
+        dadoImageView2.image = arrayDados [Int.random(in: 0...5)]
+    
+        
+    }
 }
 
